796a993f55b32773037113ddfbdf971fddd7b99a
cmanor <101116379+cmanor@users.noreply.github.com>
2022-03-22 13:40:52 -0400

    done

---
+++ index.js
@@ -0,0 +1,8 @@
+let names = ["Tracy", "Jenna", "Joco", "Amber", "Chris"];
+let foods = ["burger", "fries", "food3", "food4", "food5", "food6", "food7", "food8", "food9", "food10"];
+let mda = [
+["state1", "state2", "state3", "state4", "state5", "state6", "state7"],
+["capital1", "capital2", "capital3", "capital4", "capital5", "capital6", "capital7"],
+["bird1", "bird2", "bird3", "bird4", "bird5", "bird6", "bird7"]
+]
+console.log(names + foods + mda)
